4598de83399d2256f165ad338a1a1a1670c89d12
BrendaMwiza <brendabrizy@gmail.com>
2019-10-19 15:23:45 +0200

    image model

--- gram/models.py
+++ gram/models.py
@@ -1,10 +1,10 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
+# Create your models here.
 
-# class pro(models.Model):
-#     name = models.CharField(max_length =30)
-
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    
     
 #     def __str__(self):
 #         return self.name
