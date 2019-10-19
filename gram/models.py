0e8c78cb46cab989bcd645d002395610905dae31
BrendaMwiza <brendabrizy@gmail.com>
2019-10-19 19:05:14 +0200

    added profile model

--- gram/models.py
+++ gram/models.py
@@ -37,5 +37,41 @@ class Follower(models.Model):
     followers = models.CharField(max_length=30)
     profile = models.ForeignKey(Profile)
 
+class Profile(models.Model):
+    profile = models.ImageField(upload_to='pictures/',blank=True,null=True)
+    user_name = models.OneToOneFiled(User, on_delete=models.CASCADE,blank=True,related_name="profile")
+    boi = models.TextField(max_length=300,null=True,default="bio")
+    follower = models.ManyToManyField(User,related_name="follower",blank=True)
+    following = models.ManyToManyField(User,related_name="following",blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def following(self,follower):
+        return self.following.add(follower)
+
+    def follows(self,checkuser):
+        return checkuser in self.following.all()
+
+    def follow_numb(self):
+        if self.following.count():
+            return self.following.count()
+        else:
+            return 0
+
+    def unfollowing(self.unfollow):
+        return self.following.remove(unfollow)
+
+    @classmethod
+    def search_profile(cls,profile_item):
+        pro = cls.objects.filter(user_name__name__icontains=profile_item)
+
+    def __str__(self):
+        return self.user_name.username
+
+
 
     
