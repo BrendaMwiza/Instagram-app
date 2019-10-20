3215fa849af94ff52b26872b85aca907842c176b
BrendaMwiza <brendabrizy@gmail.com>
2019-10-20 14:56:25 +0200

    added addimage file

--- gram/urls.py
+++ gram/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name ='welcome'),
-    
+    url(r'^new/picture$', views.new_pic, name='addPic')
 ]
--- gram/views.py
+++ gram/views.py
@@ -25,3 +25,17 @@ def index(request):
         form = Form()
     return render(request, 'index.html', {"picForm":form})
 
+@login_required(login_url='/accounts/login/')
+def new_pic(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.save(commit=False)
+            picture.user_name = current_user
+            picture.save()
+        return redirect('addPic')
+
+    else:
+        form = NewImageForm()
+    return render(request, 'new_article.html', {"form": form})
