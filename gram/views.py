1627a38de0c1d321c05f6565e42e1ed25e67b725
BrendaMwiza <brendabrizy@gmail.com>
2019-10-20 13:21:03 +0200

    added a registration login file

--- gram/views.py
+++ gram/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,httpResponseRedirect
 from .models import Image,Profile,Comments,Follower
 from .email import send_welcome_email
-
+from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
 
