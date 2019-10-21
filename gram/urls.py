8cc7b788b549bf302ca6268cbb44a948880ea9de
BrendaMwiza <brendabrizy@gmail.com>
2019-10-21 12:24:22 +0200

    added remoovd bugs in profile

--- gram/urls.py
+++ gram/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.index,name ='welcome'),
     url(r'^new/picture$', views.new_pic, name='addPic'),
-    url(r'^profile$', views.getProfile, name='profile')
+    url(r'^profile$', views.getProfile, name='profile'),
+    url(r'^editprofile',views.editProfile, name='editProfile')
 ]
 
 if settings.DEBUG:
